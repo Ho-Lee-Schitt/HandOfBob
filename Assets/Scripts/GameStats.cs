031db08cae0ce8e6500aff0f3eaec66eae0af727
Niall Hughes <methos1014@gmail.com>
2016-12-11 18:33:21 +0000

    Adds internal economy and UI elements
    Adds:
     - Internal Economy (just requires addition methods)
     - Some basic UI elements.
     - Random movement of Bob (needs limits)
    
    Fixed:
     - Issue with timer not handling days correctly

---
+++ Assets/Scripts/GameStats.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class GameStats : MonoBehaviour {
+
+    public int rateOfTime;
+    public double rateOfHunger, rateOfHappiness, rateOfSocial, rateOfTiredness;
+
+    private DateTime startTime;
+    private DateTime currentTime;
+    private DateTime lastTime;
+    private double hunger;
+    private double happiness;
+    private double social;
+    private double tiredness;
+    private int duration;
+    private int day;
+
+    // Use this for initialization
+    void Start () {
+        double gameTimeInSeconds = DateTime.Now.TimeOfDay.TotalSeconds;
+
+        TimeSpan convertedTime = TimeSpan.FromSeconds(gameTimeInSeconds * rateOfTime);
+
+        convertedTime = convertedTime.Subtract(TimeSpan.FromDays(convertedTime.Days));
+
+        startTime = new DateTime(2016, 1, 1) + convertedTime;
+
+        hunger = 100;
+        happiness = 100;
+        social = 100;
+        tiredness = 100;
+
+        lastTime = startTime;
+        currentTime = startTime;
+
+        day = 1;
+
+        //gameTime.GetComponent<Text>().text = "Time: " + gameClock.ToShortTimeString();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        DateTime difference = getGameClock();
+        if (currentTime.Hour == 23 && currentTime.Minute == 59)
+        {
+            Debug.Log("");
+        }
+        currentTime = difference;
+
+        if ((currentTime.TimeOfDay - lastTime.TimeOfDay).TotalSeconds < 0)
+        {
+            day++;
+        }
+
+        double reduction = getHunger();
+        if ((hunger - reduction) > 0)
+        {
+            hunger -= reduction;
+        }
+        reduction = getHappiness();
+        if ((happiness - reduction) > 0)
+        {
+            happiness -= reduction;
+        }
+        reduction = getSocial();
+        if ((social - reduction) > 0)
+        {
+            social -= reduction;
+        }
+        reduction = getTiredness();
+        if ((tiredness - reduction) > 0)
+        {
+            tiredness -= reduction;
+        }
+
+        lastTime = currentTime;
+
+        // Debug.Log(gameClock.TimeOfDay.ToString());
+	}
+
+    DateTime getGameClock()
+    {
+        double gameTimeInSeconds = DateTime.Now.TimeOfDay.TotalSeconds;
+
+        TimeSpan convertedTime = TimeSpan.FromSeconds(gameTimeInSeconds * rateOfTime);
+        convertedTime = convertedTime.Subtract(TimeSpan.FromDays(convertedTime.Days));
+
+        DateTime gameClock = new DateTime(2016, 1, 1) + convertedTime;
+
+        return gameClock;
+    }
+
+    //DateTime convertTime(DateTime dt_toBeConvrted)
+    //{
+    //    double totalSeconds = dt_toBeConvrted.TimeOfDay.TotalSeconds;
+    //}
+
+    double getHunger()
+    {
+        double hungerValue = 100 / (rateOfHunger * 60);
+        //Debug.Log("Hunger Value " + hungerValue);
+
+        return (hungerValue * (Time.deltaTime * rateOfTime));
+    }
+
+    double getHappiness()
+    {
+        double happinessValue = 100 / (rateOfHappiness * 60);
+        //Debug.Log("Hunger Value " + happinessValue);
+
+        return (happinessValue * (Time.deltaTime * rateOfTime));
+    }
+
+    double getSocial()
+    {
+        double socialValue = 100 / (rateOfSocial * 60);
+        //Debug.Log("Hunger Value " + hungerValue);
+
+        return (socialValue * (Time.deltaTime * rateOfTime));
+    }
+
+    double getTiredness()
+    {
+        double tirednessValue = 100 / (rateOfTiredness * 60);
+        //Debug.Log("Hunger Value " + hungerValue);
+
+        return (tirednessValue * (Time.deltaTime * rateOfTime));
+    }
+
+    public int getDay()
+    {
+        return day;
+    }
+
+    public DateTime getCurrentTime()
+    {
+        return currentTime;
+    }
+
+    public double getHungerValue()
+    {
+        return hunger;
+    }
+
+    public double getHappinessValue()
+    {
+        return happiness;
+    }
+    public double getSocialValue()
+    {
+        return social;
+    }
+    public double getTirednessValue()
+    {
+        return tiredness;
+    }
+}
---
+++ Assets/Scripts/GameUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour {
+
+    public GameStats GameStat;
+    private GameObject gameTime, hungerText, happyText, socialText, TiredText;
+
+    // Use this for initialization
+    void Start () {
+        gameTime = transform.Find("Game Time").gameObject;
+        hungerText = transform.Find("Hunger Stat").gameObject;
+        happyText = transform.Find("Happy Stat").gameObject;
+        socialText = transform.Find("Social Stat").gameObject;
+        TiredText = transform.Find("Sleep Stat").gameObject;
+
+        gameTime.GetComponent<Text>().text = "Day " + GameStat.getDay() + " - " + GameStat.getCurrentTime().ToString("h:MM tt");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        gameTime.GetComponent<Text>().text = "Day " + GameStat.getDay() + " - " + GameStat.getCurrentTime().ToString("h:mm tt");
+        hungerText.GetComponent<Text>().text = "Hunger: " + GameStat.getHungerValue().ToString("F0") + "%";
+        happyText.GetComponent<Text>().text = "Happiness: " + GameStat.getHappinessValue().ToString("F0") + "%";
+        socialText.GetComponent<Text>().text = "Social: " + GameStat.getSocialValue().ToString("F0") + "%";
+        TiredText.GetComponent<Text>().text = "Tiredness: " + GameStat.getTirednessValue().ToString("F0") + "%";
+    }
+}
